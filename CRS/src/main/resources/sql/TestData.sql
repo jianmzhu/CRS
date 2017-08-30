1fd8a0cfe72ad58e90a2a5733c79125bd6b08090
xiaoming <xiaoming@windows10.microdone.cn>
2017-08-30 20:48:32 +0800

    测试数据

---
+++ CRS/src/main/resources/sql/TestData.sql
@@ -0,0 +1,10 @@
+#测试数据
+
+insert into t_car_times_dtl (car_id,times_id,book_id,book_date,book_timestamp,book_user_jobno,book_summ,book_from,book_to,book_times,check_user_jobno)
+values ('001','00:00-01:00','1000000','2017-08-30','2017-08-30 20:43:20','10000','约车去耍','公司','琶洲','00:00-01:00,01:00-02:00,02:00-03:00','10000');
+insert into t_car_times_dtl (car_id,times_id,book_id,book_date,book_timestamp,book_user_jobno,book_summ,book_from,book_to,book_times,check_user_jobno)
+values ('001','01:00-02:00','1000000','2017-08-30','2017-08-30 20:43:20','10000','约车去耍','公司','琶洲','00:00-01:00,01:00-02:00,02:00-03:00','10000');
+insert into t_car_times_dtl (car_id,times_id,book_id,book_date,book_timestamp,book_user_jobno,book_summ,book_from,book_to,book_times,check_user_jobno)
+values ('001','02:00-03:00','1000000','2017-08-30','2017-08-30 20:43:20','10000','约车去耍','公司','琶洲','00:00-01:00,01:00-02:00,02:00-03:00','10000');
+
+
