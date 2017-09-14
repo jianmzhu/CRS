a49ca75fbd87874418ad0c13999fa8fad9871b26
xiaoming <xiaoming@windows10.microdone.cn>
2017-09-14 21:59:36 +0800

    整合datatables的功能，并且添加排序支持

--- CRS/src/main/java/cn/crs/reserve/service/SysUserService.java
+++ CRS/src/main/java/cn/crs/reserve/service/SysUserService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import cn.crs.common.pagination.PagedResult;
 import cn.crs.reserve.entity.SysUser;
+import cn.crs.reserve.entity.SysUserExample;
 import cn.crs.reserve.exception.UserLoginException;
 
 public interface SysUserService {
@@ -20,6 +22,12 @@ public interface SysUserService {
 
  	// 查询所有的须知的数量
  	public int countSysUserByTypeNum();
+ 	
+ 	//按照过滤条件查询t_user记录
+ 	public List<SysUser> selectByExample(SysUserExample example);
+ 	
+ 	//按照过滤条件查询t_user记录，并且按照位移分页
+ 	public PagedResult<SysUser> selectByExampleAndOffsetPage(SysUserExample example, Integer pageOffset, Integer pageSize);
  	
  	/**
 	 * 用户登录，根据工号进行登录
